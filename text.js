a32f9e4ce2391df76567db65666b116eb0123722
Mark Hahn <mark@hahnca.com>
2022-01-23 13:58:49 -0800

    added chkPoint

--- text.js
+++ text.js
@@ -2,10 +2,13 @@ const jscad             = require('@jscad/modeling');
 const { sphere }        = jscad.primitives;
 const { vectorChar }    = jscad.text;
 const { hull }          = jscad.hulls;
-const { union }         = jscad.booleans;
-const { translate }     = jscad.maths.mat4;
 
-// check if two line segments intersect
+const strParam   = "Wyatt";
+const hullRadius = 1;
+// distance in mm each step when backing up
+const stepDistMm = 0.1;
+
+// return intersection point if exists
 // https://algs4.cs.princeton.edu/91primitives
 const intersectionPoint = (segAB, segCD) => {
   const [[Ax,Ay], [Bx,By]] = segAB;
@@ -56,7 +59,8 @@ const add = (v,w) => {
 }
 
 // http://www.fundza.com/vectors/point2line/index.html
-const distPntToLine = (pnt, start, end) => {
+const distPntToVec = (pnt, vec) => {
+  const [start, end]   = vec;
   const line_vec       = vector(start, end);
   const pnt_vec        = vector(start, pnt);
   const line_len       = length(line_vec);
@@ -64,77 +68,118 @@ const distPntToLine = (pnt, start, end) => {
   const pnt_vec_scaled = scale(pnt_vec, 1.0/line_len);
   const dt = dot(line_unitvec, pnt_vec_scaled);
   const t  = Math.max(Math.min(dt,1),0);
-  const nearest = add(scale(line_vec, t), start);
-  const dist    = distance(nearest, pnt_vec);
-  return [dist, nearest];
+  const nearestPoint = add(scale(line_vec, t), start);
+  return distance(nearestPoint, pnt_vec);
 }
 
 const showVec = (pfx, vec) => {
   console.log( pfx +
     vec[0][0].toString().padStart(2) + ','    + 
     vec[0][1].toString().padStart(2) + ' -> ' +
-    vec[1][0].toString().padStart(2)     + ','    + 
+    vec[1][0].toString().padStart(2) + ','    + 
     vec[1][1].toString().padStart(2));
 }
 
-const strParam = "Wyatt";
-
 const prevVecs  = [];
 const hulls     = [];
-let   radius    = 1;
 let   lastPoint = null;
 
 const addHull = (vec) => {
   showVec(' - hull :', vec);
-  hulls.push( hull(sphere({radius, center: vec[0].concat(0)}), 
-                   sphere({radius, center: vec[1].concat(0)})) );
+  hulls.push( 
+    hull(sphere({hullRadius, center: vec[0].concat(0)}), 
+         sphere({hullRadius, center: vec[1].concat(0)})) 
+  );
 }
 
 const addHole = (tailPoint, headPoint) => {
   showVec(' - hole :', [tailPoint, headPoint]);
 }
-const backOff = (vec) => {
-  
+
+// return point on vec far enough away from prevVec
+const backUpPoint = (prevVec, vec) => {
+  const [[Ax, Ay],[Bx, By]] = vec;
+  const vecW = Bx-Ax;
+  const vecH = By-Ay;
+  const vecLen = Math.sqrt((vecW*vecW)+(vecH*vecH));
+  for(const distOnVec = 0; distOnVec < vecLen; 
+            distOnVec += stepDistMm) {
+    const frac       = distOnVec/vecLen;
+    const trialPoint = [Ax+frac*vecW, Ay+frac*vecH];
+    const dist2prev  = distPntToVec(trialPoint, prevVec);
+    if(dist2prev > (2 * hullRadius)) return trialPoint;
+  }
+  // vec was shorter then stepDistMm
+  return null;
 }
 
-const chkBackOff = (vec) => {
+const chkPoint = (vec) => {
   for(const prevVec of prevVecs.slice(0,-1)) {
-    if(intersectionPoint(vec, prevVec)) {
-      showVec('  --X: ', prevVec);
-      vec =  [lastPoint, <-- intersection point -->];
-      // back up to point on segment far enough away
-
+    let intPt = (intersectionPoint(vec, prevVec));
+    if(intPt) {
+      showVec('  --X: ', prevVec, intPt);
+      // vec intersects a previous vector
+      // split vec in two with both vecs far enough away
+      let vec1 = null;
+      let vec2 = null;
+      const backUpPt1 = backUpPoint(prevVec, [vec[0], intPt]);
+      if(backUpPt1) vec1 = [vec[0], backUpPt1];
+      const backUpPt2 = backUpPoint(prevVec, [vec[1], intPt]);
+      if(backUpPt2) vec2 = [backUpPt2, vec[1]];
+      return {vec1, vec2};
     }
-    else {
-      const dstPntToLin = 
-              distPntToLine(point, prevVec[0], prevVec[0]);
-      if(dstPntToLin[0] < 2*radius) 
-        showVec('  --D:', dstPntToLin);
-        // back up to point on segment far enough away
-
+    const dist2prev = distPntToVec(vec[1], prevVec);
+    if(dist2prev < (2 * hullRadius)) {
+      showVec('  --D:', dstPntToLin);
+      // latest point too close to old vec
+      // back up to point on vec far enough away
+      let vec1 = null;
+      const backUpPt = backUpPoint(prevVec, vec);
+      if(backUpPt) vec1 = [vec[0], backUpPt];
+      return {vec1, vec2: null};
     }
   }
+  // point not too close to any prev vec
+  return null;
 }
 
-const handlePoint = (point, segVec2, segVecLast) => {
-  if(lastPoint) { 
-    if(segVec2) {
-      const newEndPoint = chkBackOff([lastPoint, point);
-
-      addHole(point, lastPoint);
-    }
-
-    const vec = [lastPoint, point];
-
-    showVec(' ', vec);
-
-
-    }
-    prevVecs.push(vec);
-    addHull(vec);
-    if(segVecLast) addHole(lastPoint, point);
+// returns false if point deleted
+const handlePoint = (point, segIdx, segLast) => {
+  if(lastPoint == null) { // first point
+    lastPoint = chkPoint([null, point]);
   }
-  lastPoint = point;
+  else {                   // not first point
+    let vec = [lastPoint, point];
+    // if vec too close newPoint is fractional point on vec
+    const newPoint = chkPoint(vec);
+    if(newPoint != null) { 
+      // point too close, 
+
+
+      // entire vector too close, remove it
+      if(prevVecs.length == 0) {
+        // vec is first in seg, delete seg
+        lastPoint = null;
+        return false;
+      }
+      // remove last vector in history
+      prevVecs  = prevVecs.slice(0,-1);
+      lastPoint = point;
+      vec = prevVecs.slice(-1);  // replace current vec with last
+      point = vec[0];
+    } 
+    else {
+      // point not too close
+      point = newPoint;
+      if(segIdx = 1) addHole(point, lastPoint); // add first hole
+      showVec(' ', vec);
+      prevVecs.push(vec);
+      addHull(vec);
+      if(segLast) addHole(lastPoint, point);
+      lastPoint = point;
+    }
+  }
+  return true;
 }
 
 const main = () => {
@@ -151,7 +196,12 @@ const main = () => {
       let pointIdx = 0;
       lastPoint = null;
       seg.forEach( point => {
-        handlePoint(point, pointIdx == 1, pointIdx == seg.length-1);
+        if(pointIdx == 0
+        if(!handlePoint(point, pointIdx, pointIdx == seg.length-1)) {
+          // split segment
+          pointIdx = 0;
+          return;
+        }
         pointIdx++;
       });
     });
